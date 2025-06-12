5c7f405525220c94610437019034f2198a0c24b9
sobhaan <sobhaan.ulhusan@gmail.com>
2025-06-12 14:09:22 +0200

    updated script to test animations

--- droomrobot/test/animation_test.py
+++ droomrobot/test/animation_test.py
@@ -1,6 +1,6 @@
-from sic_framework.devices.alphamini import Alphamini
-from sic_framework.devices.common_mini.mini_animation import MiniActionRequest
-
+from os.path import abspath, join
+import os
+from core import Droomrobot, AnimationType
 
 """
 Demo: Animation with alphamini.
@@ -9,20 +9,27 @@ Demo: Animation with alphamini.
 
 
 class AnimationTest:
-    def __init__(self, mini_ip, mini_id, mini_password, redis_ip):
+    def __init__(self, mini_ip, mini_id, mini_password, redis_ip,
+                 google_keyfile_path, sample_rate_dialogflow_hertz=44100, dialogflow_language="nl",
+                 google_tts_voice_name="nl-NL-Standard-D", google_tts_voice_gender="FEMALE",
+                 openai_key_path=None, default_speaking_rate=1.0,
+                 computer_test_mode=False):
             self.mini_id = mini_id
-            self.mini = Alphamini(
-                ip=mini_ip,
-                mini_id=self.mini_id,
-                mini_password=mini_password,
-                redis_ip=redis_ip
-            )
+            self.droomrobot = Droomrobot(mini_ip, mini_id, mini_password, redis_ip,
+                                     google_keyfile_path, sample_rate_dialogflow_hertz, dialogflow_language,
+                                     google_tts_voice_name, google_tts_voice_gender,
+                                     openai_key_path, default_speaking_rate,
+                                     computer_test_mode)
 
     def run(self):
-        self.mini.animation.request(MiniActionRequest(self.mini_id, "018", "action"))
-
+        self.droomrobot.animate(AnimationType.ACTION, "random_short4") ## Wave right hand
+        self.droomrobot.animate(AnimationType.EXPRESSION, "emo_007") ## Smile
+        os._exit(0)
 
 if __name__ == '__main__':
-    droomrobot = AnimationTest(mini_ip="192.168.178.111", mini_id="00167", mini_password="alphago",
-                               redis_ip="192.168.178.84")
+    droomrobot = AnimationTest(mini_ip="10.0.0.125", mini_id="00041", mini_password="mini",
+                                 redis_ip="10.0.0.109",
+                                 google_keyfile_path=abspath(join("../conf", "dialogflow", "google_keyfile.json")),
+                                 openai_key_path=abspath(join("../conf", "openai", ".openai_env")),
+                                 default_speaking_rate=0.8, computer_test_mode=False)
     droomrobot.run()
