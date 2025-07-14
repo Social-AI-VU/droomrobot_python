dedaf0a241bb341c9e4f51b5762b03fcb7a27775
Mike Ligthart <m.e.u.ligthart@vu.nl>
2025-07-14 13:57:40 +0200

    small consistency improvement when intantiating droomrobot core

--- droomrobot/droomrobot_control.py
+++ droomrobot/droomrobot_control.py
@@ -20,21 +20,25 @@ class DroomrobotControl:
         self.interaction_script = None
 
     def connect(self, mini_ip, mini_id, mini_password, redis_ip,
-                google_keyfile_path, sample_rate_dialogflow_hertz=44100, dialogflow_language="nl", dialogflow_timeout=10,
+                google_keyfile_path, sample_rate_dialogflow_hertz=44100, dialogflow_language="nl",
+                dialogflow_timeout=10.0,
                 google_tts_voice_name="nl-NL-Standard-D", google_tts_voice_gender="FEMALE", default_speaking_rate=1.0,
                 openai_key_path=None,
                 computer_test_mode=False):
 
-        self.droomrobot = Droomrobot(mini_ip, mini_id, mini_password, redis_ip, google_keyfile_path, dialogflow_timeout,
-                                     sample_rate_dialogflow_hertz, dialogflow_language, google_tts_voice_name,
-                                     google_tts_voice_gender, default_speaking_rate, openai_key_path,
-                                     computer_test_mode)
+        self.droomrobot = Droomrobot(mini_ip=mini_ip, mini_id=mini_id, mini_password=mini_password, redis_ip=redis_ip,
+                                     google_keyfile_path=google_keyfile_path, sample_rate_dialogflow_hertz=sample_rate_dialogflow_hertz,
+                                     dialogflow_language=dialogflow_language, dialogflow_timeout=dialogflow_timeout,
+                                     google_tts_voice_name=google_tts_voice_name, google_tts_voice_gender=google_tts_voice_gender,
+                                     default_speaking_rate=default_speaking_rate,
+                                     openai_key_path=openai_key_path,
+                                     computer_test_mode=computer_test_mode)
 
     def disconnect(self):
         if self.droomrobot:
             self.droomrobot.disconnect()
 
-    def start(self,participant_id: str,
+    def start(self, participant_id: str,
               interaction_context: InteractionContext,
               session: InteractionSession,
               user_model: dict):
