1876d5b254e3cddaca380ef2b079fd9fd6967e12
Mike Ligthart <m.e.u.ligthart@vu.nl>
2026-04-17 20:42:08 +0200

    ensure correct version is being installed local and on alphamine sic v2.2.1

--- droomrobot/core.py
+++ droomrobot/core.py
@@ -205,7 +205,8 @@ class Droomrobot:
                 mini_password=mini_password,
                 redis_ip=redis_ip,
                 speaker_conf=MiniSpeakersConf(sample_rate=self.sample_rate),
-                bypass_install=False
+                bypass_install=False,
+                sic_version='2.2.1'
             )
             self.speaker = self.mini.speaker
             self.mic = self.mini.mic
